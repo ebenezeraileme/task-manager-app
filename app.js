a70c8224bd9e3909da30cd74abf67cba88c89029
eben <ebenezeraileme@gmail.com>
2024-09-20 13:29:36 +0100

    mongodp config

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 
 
 
-
+app.use(express.static('./public'))
+app.use(express.urlencoded({extended: false}))
 
 app.listen(8000, ()=>{
     console.log('port is listening @ 8000')
